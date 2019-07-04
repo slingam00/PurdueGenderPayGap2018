f233e5ad14361e8bc103909e192ce913eddca78c
Sanjeev Lingam-Nattamai <slingam00@gmail.com>
2019-07-03 21:30:20 -0400

    Added Athletics Data and the summary of when the Athletics Department salaries are removed

--- SalaryData.R
+++ SalaryData.R
@@ -2,29 +2,42 @@ my_data <- read.csv("C:/Users/sling/Documents/PurdueStaffSalaries2018.csv", head
 males <- my_data[my_data$V11 == 'M',]
 m <- males$V10
 m <- as.numeric(gsub('[$,]', '', m))
-# ma <- m[m < 500000 & m > 2000]
-mean(m)
-mean(ma)
-median(m)
-median(ma)
 sd(m)
-sd(ma)
 summary(m)
-ci(m, confidence=0.95, alpha=1 - confidence, ...)
 
-head(sort(m), 350)
 females <- my_data[my_data$V11 == 'F',]
 f <- females$V10
 f <- as.numeric(gsub('[$,]', '', f))
 f
-# fe <- f[f < 500000 & f > 2000]
-mean(f)
-mean(fe)
-median(f)
-median(fe)
 sd(f)
-sd(fe)
 summary(f)
-tail(sort(f), 10)
 
-m[m > 500000]
+total<- sum(m) + sum(f)
+
+athletics <- my_data[my_data$V4 == 'WL - 4Interc Athl',]
+athletics
+a <- athletics$V10
+a <- as.numeric(gsub('[$,]', '', a))
+sum(a)
+
+sum(a) /total
+
+
+# without Athletics
+theMales <- my_data[my_data$V11 == 'M' & my_data$V4 != 'WL - 4Interc Athl',]
+theM <- theMales$V10
+theM <- as.numeric(gsub('[$,]', '', theM))
+sd(theM)
+summary(theM)
+
+theFemales <- my_data[my_data$V11 == 'F' & my_data$V4 != 'WL - 4Interc Athl',]
+theF <- theFemales$V10
+theF <- as.numeric(gsub('[$,]', '', theF))
+sum(theF)
+sd(theF)
+summary(theF)
+
+summary(m)
+sd(m)
+summary(f)
+sd(f)
